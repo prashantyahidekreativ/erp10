e7a965c6afa213d4600c5c05741d33817a43aa6b
prashantyahidekreativ <prashant.yahide@kreativtech.com>
2017-06-07 05:06:57 +0000

    website debranding

---
+++ website_debranding/README.rst
@@ -0,0 +1,12 @@
+Website debranding
+==================
+
+Removes references to odoo.com:
+
+1. Removes copyright footer
+2. Removes placeholders from social media section
+3. Replaces "Odoo" from website snippets. Replaces Image-Floating snippet
+4. Removes /website/info page
+5. Removes "Optimize your AdWords account" link
+
+Tested on Odoo 9.0 2ec9a9c99294761e56382bdcd766e90b8bc1bb38
---
+++ website_debranding/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Website debranding",
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'license': 'LGPL-3',
+    'category': 'Debranding',
+    'website': 'https://twitter.com/yelizariev',
+    'price': 60.00,
+    'currency': 'EUR',
+    'depends': ['website', 'web_debranding'],
+    'data': [
+        'views.xml',
+        ],
+    'auto_install': False,
+    'installable': True
+}
