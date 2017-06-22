4d7955f7668eac7a95fdb9779a1f4d0152b865b6
prashantyahidekreativ <prashant.yahide@kreativtech.com>
2017-06-22 12:25:12 +0000

    gst update

--- kts_gst/kts_gst.py
+++ kts_gst/kts_gst.py
@@ -6,9 +6,11 @@ from odoo import api, fields, models, _
 
 
 class kts_gst_partner(models.Model):
+    _name= 'res.partner'
     _inherit='res.partner'
+    partner_type = fields.Selection([('gst','GST'), ('uid','UID'),('gdi','GDI'),('no_app','Not Applicable')], 'Partner Type',default='no_app') 
     gstin = fields.Char('GSTIN')
-    gstin_file = fields.Binary('GSTIN File')
+    file = fields.Binary('GSTIN File')
     uid = fields.Char('UID')
     gdi = fields.Char('GDI')
 
@@ -20,12 +22,19 @@ class kts_gst_res_company(models.Model):
     composition_flag = fields.Boolean('Composition', default=False)
     manufacturer_flag = fields.Boolean('Is Manufacturer', default=False)    
     
+    @api.onchange('state_id')
+    def onchange_state(self):
+        if self.state_id:
+            self.state_code = self.state_id.code
+            self.update({'state_code':self.state_id.code})
+            return {'value':{'state_code':self.state_id.code}}    
+
 class kts_gst_account(models.Model):
     _name='kts.gst.account'
     name = fields.Char('Name')
-    cgst = fields.Float('CGST Rate')
-    igst = fields.Float('IGST Rate')
-    sgst = fields.Float('SGST Rate')
+    cgst = fields.Float('CGST Rate %')
+    igst = fields.Float('IGST Rate %')
+    sgst = fields.Float('SGST Rate %')
 
 class kts_hsn_master(models.Model):
     _name='kts.hsn.master'
@@ -52,15 +61,24 @@ class kts_gst_master(models.Model):
     cgst_out_acc_id = fields.Many2one('account.account','CGST output Account')
     sgst_in_acc_id = fields.Many2one('account.account','SGST Input Account')
     sgst_out_acc_id = fields.Many2one('account.account','SGST output Account')
-    gst_adv_in_acc_id = fields.Many2one('account.account','GST Advance Input Account')
-    gst_adv_out_acc_id = fields.Many2one('account.account','GST Advance output Account')
-    gst_paid_in_acc_id = fields.Many2one('account.account','GST Paid Input Account')
-    gst_paid_out_acc_id = fields.Many2one('account.account','GST Paid output Account')
+    
+    igst_adv_acc_id = fields.Many2one('account.account','IGST Advance Account')
+    sgst_adv_acc_id = fields.Many2one('account.account','SGST Advance  Account')
+    cgst_adv_acc_id = fields.Many2one('account.account','CGST Advance Account')
+    gst_paid_acc_id = fields.Many2one('account.account','GST Paid  Account')
 
 class kts_gst_account_tax(models.Model):
     _inherit='account.tax'
-    tax_type =  fields.Selection(selection_add=[('gst', 'GST')])
-    igst_flag = fields.Boolean('IGST',default=False)
-    cgst_flag = fields.Boolean('CGST',default=False)
-    sgst_flag = fields.Boolean('SGST',default=False)
-                         
+    tax_category =  fields.Selection(selection_add=[('gst', 'GST')])
+    gst_type = fields.Selection([('igst','IGST'),('cgst','CGST'),('sgst','SGST'), ], 'GST Type')
+    gst_account_code_id = fields.Many2one('kts.gst.account','GST Account Code')
+
+class kts_gst_fiscal_position(models.Model):
+    _inherit='account.fiscal.position'
+    tax_type = fields.Selection(selection_add=[('gst', 'GST')])
+     
+
+class kts_gst_account_configuration(models.TransientModel):
+    _inherit='account.config.settings'
+    gst_forced = fields.Boolean('GST Forced at Account',default=False)    
+                            
