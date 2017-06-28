012260faf1a60cbdc11376bd68aece0df0b31950
prashantyahidekreativ <prashant.yahide@kreativtech.com>
2017-06-28 08:21:02 +0000

    new updates

--- kts_gst/kts_gst.py
+++ kts_gst/kts_gst.py
@@ -3,7 +3,8 @@ from odoo.tools.float_utils import float_round as round
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo import api, fields, models, _
-
+from datetime import datetime
+from odoo import SUPERUSER_ID
 
 class kts_gst_partner(models.Model):
     _inherit='res.partner'
